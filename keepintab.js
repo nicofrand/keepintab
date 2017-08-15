b78c2d1b19c8d0043e22d0cf3d73b042ef97bbdd
nicofrand <mail@nicofrand.eu>
2017-08-15 16:07:20 +0200

    Do not remove named target attributes

--- keepintab.js
+++ keepintab.js
@@ -1,15 +1,21 @@
+function removeTargetIfNotFrame(element)
+{
+    let currentTarget = element.getAttribute("target");
+    if (currentTarget && (currentTarget[0] === '_')) {
+        element.removeAttribute("target");
+    }
+}
+
 function removeTargetAttributes(parent)
 {
-    Array.from(parent.querySelectorAll("a[target]")).forEach(elt => {
-        elt.removeAttribute("target");
-    });
+    Array.from(parent.querySelectorAll("a[target]")).forEach(removeTargetIfNotFrame);
 }
 
 // Create an observer instance
 let observer = new MutationObserver(mutations => {
     mutations.forEach(m => {
         if (m.type === "attributes" && m.target.hasAttribute("target")) {
-            m.target.removeAttribute("target");
+            removeTargetIfNotFrame(m.target);
         } else {
             removeTargetAttributes(m.target);
         }
